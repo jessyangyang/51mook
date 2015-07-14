427c5ab654ef07b0de9ddd66cfa598b9f4e54d08
jessyangyang <shawnisun@gmail.com>
2015-07-14 12:39:11 +0800

    wechattest

--- mook/stem/application/controllers/Test.php
+++ mook/stem/application/controllers/Test.php
@@ -15,6 +15,7 @@ use \local\common\Readability;
 use \mook\control\common\BlogManage;
 use \mook\control\admin\AdminBookManage;
 use \mook\control\common\LinkManage;
+use \local\social\WeChatSDK;
 
 
 class TestController extends \Yaf\Controller_Abstract 
@@ -125,6 +126,46 @@ class TestController extends \Yaf\Controller_Abstract
     public function phpinfoAction()
     {
         print_r(phpinfo());
+        exit();
+    }
+
+    public function wechatAction() {
+        $options = array(
+             'token'=>'krxkid', //填写你设定的key
+             'encodingaeskey'=>'2un8rwmEnk2icZnqoIR8niY2M2292zwJjxbEEyiUveL', //填写加密用的EncodingAESKey
+             'appid'=>'wx9294ba1287a3f37e', //填写高级调用功能的app id
+             'appsecret'=>'ee5cdb887bc9b61625a0c59b75f6919b' //填写高级调用功能的密钥
+        );
+
+        $weObj = new WeChatSDK($options);
+        $weObj->valid();
+        $type = $weObj->getRev()->getRevType();
+
+        switch($type) {
+            case Wechat::MSGTYPE_TEXT:
+                    $weObj->text("hello, I'm wechat")->reply();
+                    exit;
+                    break;
+            case Wechat::MSGTYPE_EVENT:
+                    break;
+            case Wechat::MSGTYPE_IMAGE:
+                break;
+            default:
+                $weObj->text("help info")->reply();
+        }
+
+        //获取菜单操作:
+        $menu = $weObj->getMenu();
+
+         //设置菜单
+        $newmenu =  array(
+            "button"=>
+            array(
+                array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
+                array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
+            )
+        );
+        $result = $weObj->createMenu($newmenu);
         exit();
     }
 }
